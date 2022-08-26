f0510a4c48984ab8f29e8fac37a8a33fde7ee7bc
Kooreis <kooreis.official@gmail.com>
2022-08-25 22:00:00 -0500

    Initial setup of the program. A dictionary named 'exchangeRates' is created to store currency codes as keys and their corresponding exchange rates as values.

--- Currency_Exchange_Tracker.cs
+++ Currency_Exchange_Tracker.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.Collections.Generic;
 
@@ -13,38 +12,5 @@ class Program
             { "JPY", 110.08m },
             { "GBP", 0.72m }
         };
-
-        while (true)
-        {
-            Console.WriteLine("Enter the base currency:");
-            string baseCurrency = Console.ReadLine().ToUpper();
-
-            if (!exchangeRates.ContainsKey(baseCurrency))
-            {
-                Console.WriteLine("Invalid base currency. Try again.");
-                continue;
-            }
-
-            Console.WriteLine("Enter the target currency:");
-            string targetCurrency = Console.ReadLine().ToUpper();
-
-            if (!exchangeRates.ContainsKey(targetCurrency))
-            {
-                Console.WriteLine("Invalid target currency. Try again.");
-                continue;
-            }
-
-            Console.WriteLine("Enter the amount:");
-            decimal amount;
-            if (!decimal.TryParse(Console.ReadLine(), out amount))
-            {
-                Console.WriteLine("Invalid amount. Try again.");
-                continue;
-            }
-
-            decimal result = amount * (exchangeRates[targetCurrency] / exchangeRates[baseCurrency]);
-            Console.WriteLine($"{amount} {baseCurrency} = {result} {targetCurrency}");
-        }
     }
 }
-```
