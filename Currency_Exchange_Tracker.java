39c891382a9ed8a4c56c53d40567465c25741404
Kooreis <kooreis.official@gmail.com>
2022-08-25 22:00:00 -0500

    In the first update, the necessary libraries are imported and the main class is created. The main method is also created, which will be the entry point of the program. The Scanner object, exchangeRate, amount, and currency variables are declared.

--- Currency_Exchange_Tracker.java
+++ Currency_Exchange_Tracker.java
@@ -1,4 +1,3 @@
-```java
 import java.util.Scanner;
 
 public class Main {
@@ -7,38 +6,5 @@ public class Main {
         double exchangeRate;
         double amount;
         String currency;
-
-        System.out.println("Welcome to Currency Exchange Tracker!");
-        System.out.println("Please enter the exchange rate from USD to your desired currency:");
-        exchangeRate = scanner.nextDouble();
-
-        System.out.println("Please enter the amount in USD you want to convert:");
-        amount = scanner.nextDouble();
-
-        System.out.println("Please enter the name of your desired currency:");
-        currency = scanner.next();
-
-        double convertedAmount = amount * exchangeRate;
-
-        System.out.println(amount + " USD is equivalent to " + convertedAmount + " " + currency + ".");
     }
 }
-
-class Currency {
-    private String name;
-    private double exchangeRate;
-
-    public Currency(String name, double exchangeRate) {
-        this.name = name;
-        this.exchangeRate = exchangeRate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getExchangeRate() {
-        return exchangeRate;
-    }
-}
-```
