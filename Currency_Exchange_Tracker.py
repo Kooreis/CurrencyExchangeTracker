ad8a67d6f05ce65356b275c9ba3f40f3c527714f
Kooreis <kooreis.official@gmail.com>
2022-08-26 23:00:00 -0500

    Defined a function to get the exchange rate between two currencies by making a GET request to the ExchangeRate-API.

--- Currency_Exchange_Tracker.py
+++ Currency_Exchange_Tracker.py
@@ -1,2 +1,4 @@
-import requests
-import json
+def get_exchange_rate(base_currency, target_currency):
+    response = requests.get(f"https://api.exchangerate-api.com/v4/latest/{base_currency}")
+    data = response.json()
+    return data["rates"][target_currency]
